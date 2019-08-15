7cd3d970e8c0c15eb91b844c82f1569f874c6c97
Nakamura Kosuke <ncl0709@gmail.com>
2019-08-16 08:54:28 +0900

    fix lint

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,53 @@
-
 export default {
-  mode: 'universal',
-  srcDir: 'app',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/element-ui'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  devModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: [/^element-ui/],
+    mode: "universal",
+    srcDir: "app",
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || "",
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            {
+                hid: "description",
+                name: "description",
+                content: process.env.npm_package_description || ""
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: "#fff" },
+    /*
+     ** Global CSS
+     */
+    css: ["element-ui/lib/theme-chalk/index.css"],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: ["@/plugins/element-ui"],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    devModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+    axios: {
+        baseURL: "https://nuxt-blog-service-c0e71.firebaseio.com"
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        transpile: [/^element-ui/],
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
